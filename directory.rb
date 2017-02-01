f5251e3f2c3f9f26e858f2151b9c97c144f32f56
Albert <ayanit1@googlemail.com>
2017-02-01 17:10:18 +0000

    Add save data to file

--- directory.rb
+++ directory.rb
@@ -30,6 +30,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -45,6 +46,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit(0)
     else
@@ -74,6 +77,18 @@ def print_footer
   else
     puts "Overall, we have #{@students.count} great students"
   end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
