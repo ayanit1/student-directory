ad80a075285afaf1f63b52e379b3f3674ded3faa
Albert <ayanit1@googlemail.com>
2017-02-01 02:59:43 +0000

    Added comment to typo.rb

--- typos.rb
+++ typos.rb
@@ -8,7 +8,7 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november} # Spell check enabled
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
